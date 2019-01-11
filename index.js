3cf9c454a20d5c41c039e739981144b7ec7f7975
sumedha <sumedhanarayanan@gmail.com>
2019-01-11 19:50:14 +0000

    A few more features of chalg

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var notes = "NPM is a easy way to program complicated stuff";
 
 var lines = "Before you write a function, just check.. is there a NPM for it?"
 
-
+var funstuff = "A fun npm is cowsay, who doesnt want a talking cow?"
 console.log(message);
 console.log(chalk.green(notes));
-console.log(chalk.magenta(lines));
+console.log(chalk.magenta.bold(lines));
+console.log(chalk.yellowBright(funstuff));
